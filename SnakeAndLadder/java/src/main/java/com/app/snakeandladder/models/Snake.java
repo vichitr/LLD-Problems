f3fcbb8401cfb268c3ad6bb0e59eaaea066661b2
Mohammed Salman <68873632+s-aLman@users.noreply.github.com>
2022-10-11 23:42:19 +0530

    updated Snake.java
    Added fields for Snake. Used two pointers one for headPosition and one for tailPosition unlike usual start and end position. The snake can bite if the player come across the head. We can place snake upwards or downwards.

--- SnakeAndLadder/java/src/main/java/com/app/snakeandladder/models/Snake.java
+++ SnakeAndLadder/java/src/main/java/com/app/snakeandladder/models/Snake.java
@@ -1,3 +1,28 @@
 package com.app.snakeandladder.models;
 
-public class Snake {}
+public class Snake {
+
+    private int headPosition;
+    private int tailPosition;
+
+    public Snake(int headPosition, int tailPosition) {
+        this.headPosition = headPosition;
+        this.tailPosition = tailPosition;
+    }
+
+    public int getHeadPosition() {
+        return headPosition;
+    }
+
+    public void setHeadPosition(int headPosition) {
+        this.headPosition = headPosition;
+    }
+
+    public int getTailPosition() {
+        return tailPosition;
+    }
+
+    public void setTailPosition(int tailPosition) {
+        this.tailPosition = tailPosition;
+    }
+}
